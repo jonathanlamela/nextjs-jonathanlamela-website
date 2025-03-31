7ecdb2c8d2de56d229c941fed8fe86bb56ec8b4e
La Mela, Jonathan <jlamela@bethel.jw.org>
2025-03-31 08:58:46 +0200

    critto util layout created

---
+++ app/mobile-apps/critto-util/privacy-policy/page.tsx
@@ -0,0 +1,49 @@
+
+export default function Home() {
+  return (
+    <main className="flex flex-col">
+      <h1 className="text-2xl">Privacy policy</h1>
+      <h2>Raccolta e utilizzo delle informazioni</h2>
+      <p><strong>Critto Util</strong> non raccoglie, memorizza, né condivide alcun dato personale degli utenti. L&apos;applicazione opera interamente in locale sul dispositivo dell&apos;utente senza trasmettere informazioni a server remoti.</p>
+
+      <h2>Permessi richiesti</h2>
+      <p>L&apos;app potrebbe richiedere alcuni permessi di sistema per il suo corretto funzionamento. Tuttavia, questi permessi non vengono utilizzati per raccogliere informazioni personali.</p>
+
+      <h2>Servizi di terze parti</h2>
+      <p>Poiché <strong>Critto Util</strong> non raccoglie dati, non condivide informazioni con servizi di terze parti.</p>
+
+      <h2>Sicurezza dei dati</h2>
+      <p>Poiché l&apos;app non raccoglie dati personali, non è necessario alcun meccanismo di protezione specifico per i dati degli utenti.</p>
+
+      <h2>Modifiche alla Privacy Policy</h2>
+      <p>Ci riserviamo il diritto di aggiornare questa Privacy Policy in qualsiasi momento. Gli utenti saranno informati di eventuali modifiche pubblicando l&apos;aggiornamento in questa sezione dell&apos;app o sul nostro sito web.</p>
+
+      <h2>Contatti</h2>
+      <p>Se hai domande sulla nostra Privacy Policy, puoi contattarci all&apos;indirizzo email: info@jonathanlamela.it</p>
+
+      <hr />
+
+      <h1>Privacy Policy of Critto Util</h1>
+      <p><strong>Last updated:</strong> [DATA]</p>
+      <p>Thank you for choosing <strong>Critto Util</strong>. Your privacy is important to us. This Privacy Policy describes how we handle user data in our application.</p>
+
+      <h2>Collection and Use of Information</h2>
+      <p><strong>Critto Util</strong> does not collect, store, or share any personal user data. The application operates entirely locally on the user&apos;s device without transmitting information to remote servers.</p>
+
+      <h2>Required Permissions</h2>
+      <p>The app may request certain system permissions for its proper functioning. However, these permissions are not used to collect personal information.</p>
+
+      <h2>Third-Party Services</h2>
+      <p>Since <strong>Critto Util</strong> does not collect data, it does not share information with third-party services.</p>
+
+      <h2>Data Security</h2>
+      <p>As the app does not collect personal data, no specific security mechanisms are required to protect user information.</p>
+
+      <h2>Changes to the Privacy Policy</h2>
+      <p>We reserve the right to update this Privacy Policy at any time. Users will be informed of any changes by publishing the update in this section of the app or on our website.</p>
+
+      <h2>Contact</h2>
+      <p>If you have any questions about our Privacy Policy, you can contact us at: info@jonathanlamela.it</p>
+    </main>
+  );
+}
