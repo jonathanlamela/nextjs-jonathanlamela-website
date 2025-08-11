187fb0e1333533282c14affca59a8bc3a87abee4
La Mela, Jonathan <jonathan.la.mela@gmail.com>
2025-08-11 21:00:24 +0200

    added calendarly

--- components/contact.tsx
+++ components/contact.tsx
@@ -12,7 +12,7 @@ export default function Contact() {
     <motion.section
       id="contact"
       ref={ref}
-      className="mb-20 sm:mb-28 w-[min(100%,38rem)]  text-center bg-amber-200 dark:bg-black p-4 rounded-4xl"
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)]  text-center  dark:bg-black p-4 rounded-4xl"
       initial={{
         opacity: 0,
       }}
@@ -28,12 +28,19 @@ export default function Contact() {
     >
       <SectionHeading >Contattami</SectionHeading>
 
-      <p className="text-gray-700 -mt-6 dark:text-white/80">
-        Contattami all&apos;indirizzo email{" "}
-        <a className="underline" href="mailto:info@jonathanlamela.it">
-          info@jonathanlamela.it
-        </a>{" "}
-      </p>
+      <div className="flex flex-col space-y-4">
+        <p className="text-gray-700 -mt-6 dark:text-white/80">
+          Contattami all&apos;indirizzo email{" "}
+          <a className="underline" href="mailto:info@jonathanlamela.it">
+            info@jonathanlamela.it
+          </a>{" "}
+        </p>
+        <p>
+          <a className="btn btn-lg btn-secondary p-4" href="https://calendly.com/jonathan-la-mela/30min" target="_blank">
+            Prenota un call gratuita di 15 minuti
+          </a>
+        </p>
+      </div>
     </motion.section>
   );
 }
