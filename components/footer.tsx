98b9a1ff3ade0b86527d6838080b3c0a853582d2
La Mela, Jonathan <jonathan.la.mela@gmail.com>
2025-08-11 21:12:31 +0200

    fixed copyright section

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2030 Jonathan La Mela
+        &copy; 2025 Jonathan La Mela
       </small>
       <div className="flex flex-col justify-center items-center">
         <Link href={"/privacy-policy/web"}>Privacy policy</Link>
